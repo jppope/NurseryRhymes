7e537c916e3aad61c40ff25de3ee5eda24ce7868
Jon Paul Uritis <jonpaul@lojistic.com>
2017-04-06 13:40:40 -0700

    ruby 99 bottles

--- 99bottles/ninetyNineBottles.js
+++ 99bottles/ninetyNineBottles.js
@@ -1,7 +1,9 @@
-//99 bottles of beer on the wall. 
+//99 bottles of beer on the wall.
 //Demonstrates classic for loop decrement
 
 console.log("99 bottles of beer on the wall");
-for (var i = 98; i > 0; i--) { console.log("take one down, pass it around," + i + " bottles of beer on the wall"); }
+for (var i = 98; i > 0; i--) {
+	console.log("take one down, pass it around," + i + " bottles of beer on the wall");
+}
 console.log("No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall...");
 
---
+++ 99bottles/ninetyNineBottles.rb
@@ -0,0 +1,11 @@
+
+# 99 bottles of beer on the wall.
+# Demonstrates classic for loop decrement
+
+puts '99 bottles of beer on the wall'
+
+for i in (99).downto(1)
+  puts "take one down, pass it around, #{i} bottles of beer on the wall"
+end
+
+puts "No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall..."
